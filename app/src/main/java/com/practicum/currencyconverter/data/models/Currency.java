ce3cb8f1586c24d79ea0d05e872d20636c735a8e
s-buvaka <s-buvaka@yandex-team.ru>
2022-03-07 00:40:35 +0300

    Remove unused currencies

--- app/src/main/java/com/practicum/currencyconverter/data/models/Currency.java
+++ app/src/main/java/com/practicum/currencyconverter/data/models/Currency.java
@@ -6,167 +6,26 @@ import androidx.annotation.DrawableRes;
 import androidx.annotation.StringRes;
 
 public enum Currency {
-    AED("AED", R.string.currency_uae_dirham , R.drawable.ic_launcher_background), // UAE Dirham	United Arab Emirates
-    AFN("AFN", R.string.currency_afghan_afghani , R.drawable.ic_launcher_background), // Afghan Afghani	Afghanistan
-    ALL("ALL", R.string.currency_albania , R.drawable.ic_launcher_background), // Albanian Lek	Albania
-    AMD("AMD", R.string.currency_armenia , R.drawable.ic_launcher_background), // Armenian Dram	Armenia
-    ANG("ANG", R.string.currency_netherlands_antilles , R.drawable.ic_launcher_background), // Netherlands Antillian Guilder	Netherlands Antilles
-    AOA("AOA", R.string.currency_angola , R.drawable.ic_launcher_background), // Angolan Kwanza	Angola
-    ARS("ARS", R.string.currency_argentina , R.drawable.ic_launcher_background), // Argentine Peso	Argentina
-    AUD("AUD", R.string.currency_australia , R.drawable.ic_launcher_background), // Australian Dollar	Australia
-    AWG("AWG", R.string.currency_aruba , R.drawable.ic_launcher_background), // Aruban Florin	Aruba
-    AZN("AZN", R.string.currency_azerbaijan , R.drawable.ic_launcher_background), // Azerbaijani Manat	Azerbaijan
-    BAM("BAM", R.string.currency_bosnia_herzegovina , R.drawable.ic_launcher_background), // Bosnia and Herzegovina Mark	Bosnia and Herzegovina
-    BBD("BBD", R.string.currency_barbados , R.drawable.ic_launcher_background), // Barbados Dollar	Barbados
-    BDT("BDT", R.string.currency_bangladesh , R.drawable.ic_launcher_background), // Bangladeshi Taka	Bangladesh
-    BGN("BGN", R.string.currency_bulgaria , R.drawable.ic_launcher_background), // Bulgarian Lev	Bulgaria
-    BHD("BHD", R.string.currency_bahrain , R.drawable.ic_launcher_background), // Bahraini Dinar	Bahrain
-    BIF("BIF", R.string.currency_burundi , R.drawable.ic_launcher_background), // Burundian Franc	Burundi
-    BMD("BMD", R.string.currency_bermuda , R.drawable.ic_launcher_background), // Bermudian Dollar	Bermuda
-    BND("BND", R.string.currency_brunei , R.drawable.ic_launcher_background), // Brunei Dollar	Brunei
-    BOB("BOB", R.string.currency_bolivia , R.drawable.ic_launcher_background), // Bolivian Boliviano	Bolivia
-    BRL("BRL", R.string.currency_brazil , R.drawable.ic_launcher_background), // Brazilian Real	Brazil
-    BSD("BSD", R.string.currency_bahamas , R.drawable.ic_launcher_background), // Bahamian Dollar	Bahamas
-    BTN("BTN", R.string.currency_bhutan , R.drawable.ic_launcher_background), // Bhutanese Ngultrum	Bhutan
-    BWP("BWP", R.string.currency_botswana , R.drawable.ic_launcher_background), // Botswana Pula	Botswana
-    BYN("BYN", R.string.currency_belarus , R.drawable.ic_launcher_background), // Belarusian Ruble	Belarus
-    BZD("BZD", R.string.currency_belize , R.drawable.ic_launcher_background), // Belize Dollar	Belize
-    CAD("CAD", R.string.currency_canada , R.drawable.ic_launcher_background), // Canadian Dollar	Canada
-    CDF("CDF", R.string.currency_republic_congo , R.drawable.ic_launcher_background), // Congolese Franc	Democratic Republic of the Congo
-    CHF("CHF", R.string.currency_switzerland , R.drawable.ic_launcher_background), // Swiss Franc	Switzerland
-    CLP("CLP", R.string.currency_chile , R.drawable.ic_launcher_background), // Chilean Peso	Chile
-    CNY("CNY", R.string.currency_china , R.drawable.ic_launcher_background), // Chinese Renminbi	China
-    COP("COP", R.string.currency_colombia , R.drawable.ic_launcher_background), // Colombian Peso	Colombia
-    CRC("CRC", R.string.currency_costa_rica , R.drawable.ic_launcher_background), // Costa Rican Colon	Costa Rica
-    CUP("CUP", R.string.currency_cuba , R.drawable.ic_launcher_background), // Cuban Peso	Cuba
-    CVE("CVE", R.string.currency_cape_verde , R.drawable.ic_launcher_background), // Cape Verdean Escudo	Cape Verde
-    CZK("CZK", R.string.currency_czech_republic , R.drawable.ic_launcher_background), // Czech Koruna	Czech Republic
-    DJF("DJF", R.string.currency_djibouti , R.drawable.ic_launcher_background), // Djiboutian Franc	Djibouti
-    DKK("DKK", R.string.currency_denmark , R.drawable.ic_launcher_background), // Danish Krone	Denmark
-    DOP("DOP", R.string.currency_dominican_republic , R.drawable.ic_launcher_background), // Dominican Peso	Dominican Republic
-    DZD("DZD", R.string.currency_algeria , R.drawable.ic_launcher_background), // Algerian Dinar	Algeria
-    EGP("EGP", R.string.currency_egypt , R.drawable.ic_launcher_background), // Egyptian Pound	Egypt
-    ERN("ERN", R.string.currency_eritrea , R.drawable.ic_launcher_background), // Eritrean Nakfa	Eritrea
-    ETB("ETB", R.string.currency_ethiopia , R.drawable.ic_launcher_background), // Ethiopian Birr	Ethiopia
-    EUR("EUR", R.string.currency_european_union , R.drawable.ic_launcher_background), // Euro	European Union
-    FJD("FJD", R.string.currency_fiji , R.drawable.ic_launcher_background), // Fiji Dollar	Fiji
-    FKP("FKP", R.string.currency_falkland_islands , R.drawable.ic_launcher_background), // Falkland Islands Pound	Falkland Islands
-    FOK("FOK", R.string.currency_faroe_islands , R.drawable.ic_launcher_background), // Faroese Króna	Faroe Islands
-    GBP("GBP", R.string.currency_united_kingdom , R.drawable.ic_launcher_background), // Pound Sterling	United Kingdom
-    GEL("GEL", R.string.currency_georgia , R.drawable.ic_launcher_background), // Georgian Lari	Georgia
-    GGP("GGP", R.string.currency_guernsey , R.drawable.ic_launcher_background), // Guernsey Pound	Guernsey
-    GHS("GHS", R.string.currency_ghana , R.drawable.ic_launcher_background), // Ghanaian Cedi	Ghana
-    GIP("GIP", R.string.currency_gibraltar , R.drawable.ic_launcher_background), // Gibraltar Pound	Gibraltar
-    GMD("GMD", R.string.currency_the_gambia , R.drawable.ic_launcher_background), // Gambian Dalasi	The Gambia
-    GNF("GNF", R.string.currency_guinea , R.drawable.ic_launcher_background), // Guinean Franc	Guinea
-    GTQ("GTQ", R.string.currency_guatemala , R.drawable.ic_launcher_background), // Guatemalan Quetzal	Guatemala
-    GYD("GYD", R.string.currency_guyana , R.drawable.ic_launcher_background), // Guyanese Dollar	Guyana
-    HKD("HKD", R.string.currency_hong_kong , R.drawable.ic_launcher_background), // Hong Kong Dollar	Hong Kong
-    HNL("HNL", R.string.currency_honduras , R.drawable.ic_launcher_background), // Honduran Lempira	Honduras
-    HRK("HRK", R.string.currency_croatia , R.drawable.ic_launcher_background), // Croatian Kuna	Croatia
-    HTG("HTG", R.string.currency_haiti , R.drawable.ic_launcher_background), // Haitian Gourde	Haiti
-    HUF("HUF", R.string.currency_hungary , R.drawable.ic_launcher_background), // Hungarian Forint	Hungary
-    IDR("IDR", R.string.currency_indonesia , R.drawable.ic_launcher_background), // Indonesian Rupiah	Indonesia
-    ILS("ILS", R.string.currency_israel , R.drawable.ic_launcher_background), // Israeli New Shekel	Israel
-    IMP("IMP", R.string.currency_isle_of_man , R.drawable.ic_launcher_background), // Manx Pound	Isle of Man
-    INR("INR", R.string.currency_india , R.drawable.ic_launcher_background), // Indian Rupee	India
-    IQD("IQD", R.string.currency_iraq , R.drawable.ic_launcher_background), // Iraqi Dinar	Iraq
-    IRR("IRR", R.string.currency_iran , R.drawable.ic_launcher_background), // Iranian Rial	Iran
-    ISK("ISK", R.string.currency_iceland , R.drawable.ic_launcher_background), // Icelandic Króna	Iceland
-    JEP("JEP", R.string.currency_jersey , R.drawable.ic_launcher_background), // Jersey Pound	Jersey
-    JMD("JMD", R.string.currency_jamaica , R.drawable.ic_launcher_background), // Jamaican Dollar	Jamaica
-    JOD("JOD", R.string.currency_jordan , R.drawable.ic_launcher_background), // Jordanian Dinar	Jordan
-    JPY("JPY", R.string.currency_japan , R.drawable.ic_launcher_background), // Japanese Yen	Japan
-    KES("KES", R.string.currency_kenya , R.drawable.ic_launcher_background), // Kenyan Shilling	Kenya
-    KGS("KGS", R.string.currency_kyrgyzstan , R.drawable.ic_launcher_background), // Kyrgyzstani Som	Kyrgyzstan
-    KHR("KHR", R.string.currency_cambodia , R.drawable.ic_launcher_background), // Cambodian Riel	Cambodia
-    KID("KID", R.string.currency_kiribati , R.drawable.ic_launcher_background), // Kiribati Dollar	Kiribati
-    KMF("KMF", R.string.currency_comoros , R.drawable.ic_launcher_background), // Comorian Franc	Comoros
-    KRW("KRW", R.string.currency_south_korea , R.drawable.ic_launcher_background), // South Korean Won	South Korea
-    KWD("KWD", R.string.currency_kuwait , R.drawable.ic_launcher_background), // Kuwaiti Dinar	Kuwait
-    KYD("KYD", R.string.currency_cayman_islands , R.drawable.ic_launcher_background), // Cayman Islands Dollar	Cayman Islands
-    KZT("KZT", R.string.currency_kazakhstan , R.drawable.ic_launcher_background), // Kazakhstani Tenge	Kazakhstan
-    LAK("LAK", R.string.currency_laos , R.drawable.ic_launcher_background), // Lao Kip	Laos
-    LBP("LBP", R.string.currency_lebanon , R.drawable.ic_launcher_background), // Lebanese Pound	Lebanon
-    LKR("LKR", R.string.currency_sri_lanka , R.drawable.ic_launcher_background), // Sri Lanka Rupee	Sri Lanka
-    LRD("LRD", R.string.currency_liberia , R.drawable.ic_launcher_background), // Liberian Dollar	Liberia
-    LSL("LSL", R.string.currency_lesotho , R.drawable.ic_launcher_background), // Lesotho Loti	Lesotho
-    LYD("LYD", R.string.currency_libya , R.drawable.ic_launcher_background), // Libyan Dinar	Libya
-    MAD("MAD", R.string.currency_morocco , R.drawable.ic_launcher_background), // Moroccan Dirham	Morocco
-    MDL("MDL", R.string.currency_moldova , R.drawable.ic_launcher_background), // Moldovan Leu	Moldova
-    MGA("MGA", R.string.currency_madagascar , R.drawable.ic_launcher_background), // Malagasy Ariary	Madagascar
-    MKD("MKD", R.string.currency_north_macedonia , R.drawable.ic_launcher_background), // Macedonian Denar	North Macedonia
-    MMK("MMK", R.string.currency_myanmar , R.drawable.ic_launcher_background), // Burmese Kyat	Myanmar
-    MNT("MNT", R.string.currency_mongolia , R.drawable.ic_launcher_background), // Mongolian Tögrög	Mongolia
-    MOP("MOP", R.string.currency_macau , R.drawable.ic_launcher_background), // Macanese Pataca	Macau
-    MRU("MRU", R.string.currency_mauritania , R.drawable.ic_launcher_background), // Mauritanian Ouguiya	Mauritania
-    MUR("MUR", R.string.currency_mauritius , R.drawable.ic_launcher_background), // Mauritian Rupee	Mauritius
-    MVR("MVR", R.string.currency_maldives , R.drawable.ic_launcher_background), // Maldivian Rufiyaa	Maldives
-    MWK("MWK", R.string.currency_malawi , R.drawable.ic_launcher_background), // Malawian Kwacha	Malawi
-    MXN("MXN", R.string.currency_mexico , R.drawable.ic_launcher_background), // Mexican Peso	Mexico
-    MYR("MYR", R.string.currency_malaysia , R.drawable.ic_launcher_background), // Malaysian Ringgit	Malaysia
-    MZN("MZN", R.string.currency_mozambique , R.drawable.ic_launcher_background), // Mozambican Metical	Mozambique
-    NAD("NAD", R.string.currency_namibia , R.drawable.ic_launcher_background), // Namibian Dollar	Namibia
-    NGN("NGN", R.string.currency_nigeria , R.drawable.ic_launcher_background), // Nigerian Naira	Nigeria
-    NIO("NIO", R.string.currency_nicaragua , R.drawable.ic_launcher_background), // Nicaraguan Córdoba	Nicaragua
-    NOK("NOK", R.string.currency_norway , R.drawable.ic_launcher_background), // Norwegian Krone	Norway
-    NPR("NPR", R.string.currency_nepal , R.drawable.ic_launcher_background), // Nepalese Rupee	Nepal
-    NZD("NZD", R.string.currency_new_zealand , R.drawable.ic_launcher_background), // New Zealand Dollar	New Zealand
-    OMR("OMR", R.string.currency_oman , R.drawable.ic_launcher_background), // Omani Rial	Oman
-    PAB("PAB", R.string.currency_panama , R.drawable.ic_launcher_background), // Panamanian Balboa	Panama
-    PEN("PEN", R.string.currency_peru , R.drawable.ic_launcher_background), // Peruvian Sol	Peru
-    PGK("PGK", R.string.currency_papua_new_guinea , R.drawable.ic_launcher_background), // Papua New Guinean Kina	Papua New Guinea
-    PHP("PHP", R.string.currency_philippines , R.drawable.ic_launcher_background), // Philippine Peso	Philippines
-    PKR("PKR", R.string.currency_pakistan , R.drawable.ic_launcher_background), // Pakistani Rupee	Pakistan
-    PLN("PLN", R.string.currency_poland , R.drawable.ic_launcher_background), // Polish Złoty	Poland
-    PYG("PYG", R.string.currency_paraguay , R.drawable.ic_launcher_background), // Paraguayan Guaraní	Paraguay
-    QAR("QAR", R.string.currency_qatar , R.drawable.ic_launcher_background), // Qatari Riyal	Qatar
-    RON("RON", R.string.currency_romania , R.drawable.ic_launcher_background), // Romanian Leu	Romania
-    RSD("RSD", R.string.currency_serbia , R.drawable.ic_launcher_background), // Serbian Dinar	Serbia
-    RUB("RUB", R.string.currency_russia , R.drawable.ic_launcher_background), // Russian Ruble	Russia
-    RWF("RWF", R.string.currency_rwanda , R.drawable.ic_launcher_background), // Rwandan Franc	Rwanda
-    SAR("SAR", R.string.currency_saudi_arabia , R.drawable.ic_launcher_background), // Saudi Riyal	Saudi Arabia
-    SBD("SBD", R.string.currency_solomon_islands , R.drawable.ic_launcher_background), // Solomon Islands Dollar	Solomon Islands
-    SCR("SCR", R.string.currency_seychelles , R.drawable.ic_launcher_background), // Seychellois Rupee	Seychelles
-    SDG("SDG", R.string.currency_sudan , R.drawable.ic_launcher_background), // Sudanese Pound	Sudan
-    SEK("SEK", R.string.currency_sweden , R.drawable.ic_launcher_background), // Swedish Krona	Sweden
-    SGD("SGD", R.string.currency_singapore , R.drawable.ic_launcher_background), // Singapore Dollar	Singapore
-    SHP("SHP", R.string.currency_saint_helena , R.drawable.ic_launcher_background), // Saint Helena Pound	Saint Helena
-    SLL("SLL", R.string.currency_sierra_leone , R.drawable.ic_launcher_background), // Sierra Leonean Leone	Sierra Leone
-    SOS("SOS", R.string.currency_somalia , R.drawable.ic_launcher_background), // Somali Shilling	Somalia
-    SRD("SRD", R.string.currency_suriname , R.drawable.ic_launcher_background), // Surinamese Dollar	Suriname
-    SSP("SSP", R.string.currency_south_sudan , R.drawable.ic_launcher_background), // South Sudanese Pound	South Sudan
-    STN("STN", R.string.currency_são_tomé_príncipe , R.drawable.ic_launcher_background), // São Tomé and Príncipe Dobra	São Tomé and Príncipe
-    SYP("SYP", R.string.currency_syria , R.drawable.ic_launcher_background), // Syrian Pound	Syria
-    SZL("SZL", R.string.currency_eswatini , R.drawable.ic_launcher_background), // Eswatini Lilangeni	Eswatini
-    THB("THB", R.string.currency_thailand , R.drawable.ic_launcher_background), // Thai Baht	Thailand
-    TJS("TJS", R.string.currency_tajikistan , R.drawable.ic_launcher_background), // Tajikistani Somoni	Tajikistan
-    TMT("TMT", R.string.currency_turkmenistan , R.drawable.ic_launcher_background), // Turkmenistan Manat	Turkmenistan
-    TND("TND", R.string.currency_tunisia , R.drawable.ic_launcher_background), // Tunisian Dinar	Tunisia
-    TOP("TOP", R.string.currency_tonga , R.drawable.ic_launcher_background), // Tongan Paʻanga	Tonga
-    TRY("TRY", R.string.currency_turkey , R.drawable.ic_launcher_background), // Turkish Lira	Turkey
-    TTD("TTD", R.string.currency_trinidad_tobago , R.drawable.ic_launcher_background), // Trinidad and Tobago Dollar	Trinidad and Tobago
-    TVD("TVD", R.string.currency_tuvalu , R.drawable.ic_launcher_background), // Tuvaluan Dollar	Tuvalu
-    TWD("TWD", R.string.currency_taiwan , R.drawable.ic_launcher_background), // New Taiwan Dollar	Taiwan
-    TZS("TZS", R.string.currency_tanzania , R.drawable.ic_launcher_background), // Tanzanian Shilling	Tanzania
-    UAH("UAH", R.string.currency_ukraine , R.drawable.ic_launcher_background), // Ukrainian Hryvnia	Ukraine
-    UGX("UGX", R.string.currency_uganda , R.drawable.ic_launcher_background), // Ugandan Shilling	Uganda
-    USD("USD", R.string.currency_united_states , R.drawable.ic_launcher_background), // United States Dollar	United States
-    UYU("UYU", R.string.currency_uruguay , R.drawable.ic_launcher_background), // Uruguayan Peso	Uruguay
-    UZS("UZS", R.string.currency_uzbekistan , R.drawable.ic_launcher_background), // Uzbekistani So'm	Uzbekistan
-    VES("VES", R.string.currency_venezuela , R.drawable.ic_launcher_background), // Venezuelan Bolívar Soberano	Venezuela
-    VND("VND", R.string.currency_vietnam , R.drawable.ic_launcher_background), // Vietnamese Đồng	Vietnam
-    VUV("VUV", R.string.currency_vanuatu , R.drawable.ic_launcher_background), // Vanuatu Vatu	Vanuatu
-    WST("WST", R.string.currency_samoa , R.drawable.ic_launcher_background), // Samoan Tālā	Samoa
-    XAF("XAF", R.string.currency_cemac , R.drawable.ic_launcher_background), // Central African CFA Franc	CEMAC
-    XCD("XCD", R.string.currency_eastern_caribbean_states , R.drawable.ic_launcher_background), // East Caribbean Dollar	Organisation of Eastern Caribbean States
-    XDR("XDR", R.string.currency_international_monetary_fund , R.drawable.ic_launcher_background), // Special Drawing Rights	International Monetary Fund
-    XOF("XOF", R.string.currency_cfa , R.drawable.ic_launcher_background), // West African CFA franc	CFA
-    XPF("XPF", R.string.currency_doutre_mer , R.drawable.ic_launcher_background), // CFP Franc	Collectivités d'Outre-Mer
-    YER("YER", R.string.currency_yemen , R.drawable.ic_launcher_background), // Yemeni Rial	Yemen
-    ZAR("ZAR", R.string.currency_south_africa , R.drawable.ic_launcher_background), // South African Rand	South Africa
-    ZMW("ZMW", R.string.currency_zambia , R.drawable.ic_launcher_background), // Zambian Kwacha	Zambia
-    ZWL("ZWL", R.string.currency_zimbabwe , R.drawable.ic_launcher_background); // Zimbabwean Dollar	Zimbabwe
+    AUD("AUD", R.string.currency_australia, R.drawable.ic_australia),
+    CAD("CAD", R.string.currency_canada, R.drawable.ic_canada),
+    CHF("CHF", R.string.currency_switzerland, R.drawable.ic_switzerland),
+    CNY("CNY", R.string.currency_china, R.drawable.ic_china),
+    CZK("CZK", R.string.currency_czech_republic, R.drawable.ic_czech_republic),
+    DKK("DKK", R.string.currency_denmark, R.drawable.ic_denmark),
+    EUR("EUR", R.string.currency_european_union, R.drawable.ic_euro),
+    GBP("GBP", R.string.currency_united_kingdom, R.drawable.ic_great_britain),
+    HKD("HKD", R.string.currency_hong_kong, R.drawable.ic_hong_kong),
+    JPY("JPY", R.string.currency_japan, R.drawable.ic_japan),
+    MXN("MXN", R.string.currency_mexico, R.drawable.ix_mexico),
+    NOK("NOK", R.string.currency_norway, R.drawable.ic_norway),
+    NZD("NZD", R.string.currency_new_zealand, R.drawable.ic_new_zeland),
+    PLN("PLN", R.string.currency_poland, R.drawable.ic_poland),
+    RUB("RUB", R.string.currency_russia, R.drawable.ic_russia),
+    SEK("SEK", R.string.currency_sweden, R.drawable.ic_sweden),
+    SGD("SGD", R.string.currency_singapore, R.drawable.ic_singapore),
+    TRY("TRY", R.string.currency_turkey, R.drawable.ic_turkey),
+    USD("USD", R.string.currency_united_states, R.drawable.ic_usa),
+    ZAR("ZAR", R.string.currency_south_africa, R.drawable.ic_south_africa);
 
     private final String code;
 
