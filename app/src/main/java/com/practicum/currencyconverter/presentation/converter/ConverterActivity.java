d5cca1f1283fff35da3037acf4ab6f07d64c8079
s-buvaka <s-buvaka@yandex-team.ru>
2022-03-07 21:19:43 +0300

    handle some ui actions

--- app/src/main/java/com/practicum/currencyconverter/presentation/converter/ConverterActivity.java
+++ app/src/main/java/com/practicum/currencyconverter/presentation/converter/ConverterActivity.java
@@ -1,7 +1,11 @@
 package com.practicum.currencyconverter.presentation.converter;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Pair;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 import com.practicum.currencyconverter.data.models.Currency;
 import com.practicum.currencyconverter.databinding.ActivityConverterBinding;
@@ -21,7 +25,6 @@ public class ConverterActivity extends AppCompatActivity {
     private ActivityConverterBinding binding;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityConverterBinding.inflate(getLayoutInflater());
@@ -29,19 +32,31 @@ public class ConverterActivity extends AppCompatActivity {
 
         initView();
         initViewModel();
-
-        converterViewModel.getCurrencyRate(Currency.USD, Currency.RUB);
+        openKeyBoard();
     }
 
     private void initView() {
         binding.fromCurrencyIconImageView.setOnClickListener(v -> openCurrencies(CurrencyInput.FROM));
         binding.toCurrencyIconImageView.setOnClickListener(v -> openCurrencies(CurrencyInput.TO));
+        binding.swapFab.setOnClickListener(v -> {
+            converterViewModel.swapCurrencies();
+            swapCurrencyInputs();
+        });
+        binding.refreshButton.setOnClickListener(v -> clearFields());
     }
 
     private void initViewModel() {
         converterViewModel = new ViewModelProvider(this).get(ConverterViewModel.class);
         converterViewModel.getPairConversationLiveData().observe(this, pairConversationDto -> {
         });
+
+        converterViewModel.getFromCurrencyLiveData().observe(this, this::setFromCurrency);
+        converterViewModel.getToCurrencyLiveData().observe(this, this::setToCurrency);
+    }
+
+    private void openKeyBoard() {
+        binding.fromInputEditText.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
 
     private void openCurrencies(final CurrencyInput currencyInput) {
@@ -54,13 +69,34 @@ public class ConverterActivity extends AppCompatActivity {
 
         switch (currencyInput) {
             case FROM:
-                binding.fromCurrencyIconImageView.setImageDrawable(ContextCompat.getDrawable(this, currency.getIcon()));
-                binding.fromCurrencyCodeTextView.setText(currency.getCode());
+                converterViewModel.setFromCurrency(currency);
                 break;
             case TO:
-                binding.toCurrencyIconImageView.setImageDrawable(ContextCompat.getDrawable(this, currency.getIcon()));
-                binding.toCurrencyCodeTextView.setText(currency.getCode());
+                converterViewModel.setToCurrency(currency);
                 break;
         }
+    }
+
+    private void setFromCurrency(final Currency currency) {
+        binding.fromCurrencyIconImageView.setImageDrawable(ContextCompat.getDrawable(this, currency.getIcon()));
+        binding.fromCurrencyCodeTextView.setText(currency.getCode());
+    }
+
+    private void setToCurrency(final Currency currency) {
+        binding.toCurrencyIconImageView.setImageDrawable(ContextCompat.getDrawable(this, currency.getIcon()));
+        binding.toCurrencyCodeTextView.setText(currency.getCode());
+    }
+
+    private void swapCurrencyInputs() {
+        final Editable from = binding.fromInputEditText.getText();
+        final Editable to = binding.toInputEditText.getText();
+
+        binding.fromInputEditText.setText(to);
+        binding.toInputEditText.setText(from);
+    }
+
+    private void clearFields() {
+        binding.fromInputEditText.setText("");
+        binding.toInputEditText.setText("");
     }
 }
--- app/src/main/java/com/practicum/currencyconverter/presentation/converter/ConverterViewModel.java
+++ app/src/main/java/com/practicum/currencyconverter/presentation/converter/ConverterViewModel.java
@@ -17,9 +17,19 @@ public class ConverterViewModel extends BaseViewModel {
     private final ExchangerRateService exchangerRateService = DI.exchangerRateService();
 
     private final MutableLiveData<PairConversation> pairConversationLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Currency> fromCurrencyLiveData = new MutableLiveData<>(Currency.USD);
+    private final MutableLiveData<Currency> toCurrencyLiveData = new MutableLiveData<>(Currency.RUB);
 
     public LiveData<PairConversation> getPairConversationLiveData() {
         return pairConversationLiveData;
+    }
+
+    public LiveData<Currency> getFromCurrencyLiveData() {
+        return fromCurrencyLiveData;
+    }
+
+    public LiveData<Currency> getToCurrencyLiveData() {
+        return toCurrencyLiveData;
     }
 
     public void getCurrencyRate(final Currency from, final Currency to) {
@@ -33,5 +43,21 @@ public class ConverterViewModel extends BaseViewModel {
             public void onFailure(final Call<PairConversation> call, final Throwable t) {
             }
         });
+    }
+
+    public void setFromCurrency(final Currency currency) {
+        fromCurrencyLiveData.postValue(currency);
+    }
+
+    public void setToCurrency(final Currency currency) {
+        toCurrencyLiveData.postValue(currency);
+    }
+
+    public void swapCurrencies() {
+        final Currency from = fromCurrencyLiveData.getValue();
+        final Currency to = toCurrencyLiveData.getValue();
+
+        fromCurrencyLiveData.postValue(to);
+        toCurrencyLiveData.postValue(from);
     }
 }
